3a7e065737af4828af6dbc0cc2ccb3b40a548872
jackphi2023 <jackphi2023@gmail.com>
2026-08-11 17:14:40 +0700

    LX-05 scope lint exception to module augmentation

--- apps/mobile/src/lib/style-sheet-compat.ts
+++ apps/mobile/src/lib/style-sheet-compat.ts
@@ -1,6 +1,8 @@
 import { StyleSheet } from 'react-native';
 
 declare module 'react-native' {
+  // TypeScript module augmentation requires this namespace to extend React Native's StyleSheet type.
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace StyleSheet {
     const absoluteFillObject: typeof absoluteFill;
   }
