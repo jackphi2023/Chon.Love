f268bac0d9087127c2fb84ecdca63c09e90d2ff0
jackphi2023 <jackphi2023@gmail.com>
2026-08-15 16:44:28 +0700

    Allow Metro asset resolver for web logo

--- apps/mobile/src/components/chon-love-logo.web.tsx
+++ apps/mobile/src/components/chon-love-logo.web.tsx
@@ -1,6 +1,8 @@
 import type { CSSProperties } from 'react';
 import { Image as ReactNativeImage, StyleSheet, type ImageStyle, type StyleProp } from 'react-native';
 
+// Metro returns the environment-correct bundled URL; a static root path is not reliable in Expo web/Netlify.
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const logoUri = ReactNativeImage.resolveAssetSource(require('../../public/chonlove-logo.webp')).uri;
 
 export function ChonLoveLogo({
