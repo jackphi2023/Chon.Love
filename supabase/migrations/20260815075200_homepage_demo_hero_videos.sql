7fc74aa239992938fa8b1510f88d0b5a792b985d
jackphi2023 <jackphi2023@gmail.com>
2026-08-15 14:53:45 +0700

    Align homepage demo migration with remote ledger

--- supabase/migrations/20260815075200_homepage_demo_hero_videos.sql
+++
@@ -1,8 +0,0 @@
-update public.homepage_settings
-set hero_desktop_youtube_url = 'https://www.youtube.com/watch?v=zWPQNc4tRxg',
-    hero_mobile_youtube_url = 'https://www.youtube.com/shorts/rGiUkadX-xk',
-    updated_at = now(),
-    updated_by = null
-where id = 1
-  and hero_desktop_youtube_url is null
-  and hero_mobile_youtube_url is null;
